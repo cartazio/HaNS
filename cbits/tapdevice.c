2a594edd5463d7ff0f2972dce7805f0dede13d9d
Evan Cofsky <evan@tunixman.com>
2012-12-12 21:03:54 -0800

    Hack support for OS X.

--- cbits/tapdevice.c
+++ cbits/tapdevice.c
@@ -6,12 +6,19 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+
+#ifdef __linux__
 #include <linux/if.h>
 #include <linux/if_tun.h>
+#endif
 
 int init_tap_device(char *name) {
-	int fd, ret;
+	int fd;
+
+#ifdef __linux__
+        int ret;
 	struct ifreq ifr;
+#endif
 
 	if(name == NULL) {
 		return -1;
@@ -22,15 +29,17 @@ int init_tap_device(char *name) {
 		return -2;
 	}
 
+#ifdef __linux__        
 	memset(&ifr, 0x0, sizeof(struct ifreq));
+
 	ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
 	strncpy(ifr.ifr_name, name, IFNAMSIZ);
-
 	ret = ioctl(fd, TUNSETIFF, (void*) &ifr);
 	if(ret != 0) {
 		close(fd);
 		return -3;
 	}
+#endif
 
 	return fd;
 }
